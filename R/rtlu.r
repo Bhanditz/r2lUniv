3be03840432156d63f3b5a3b8757f7f4a1806bad
Christophe Genolini <genolini@u-paris10.fr>
2009-01-29 00:00:00 +0000

    version 0.9.2

--- R/rtlu.r
+++ R/rtlu.r
@@ -1,4 +1,4 @@
-rtlu <- function(data,fileOutput="",textBefore="",graphDir="graphUniv",graphName="V",...){
+rtlu <- function(data,fileOutput="",textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     if(fileOutput!=""){
         sink(fileOutput)
         on.exit(sink())
@@ -6,31 +6,32 @@ rtlu <- function(data,fileOutput="",textBefore="",graphDir="graphUniv",graphName
     if(graphDir!=""){
         dir.create(graphDir,showWarnings=FALSE)
     }else{}
-    r2lUniv(data=data,textBefore=textBefore,graphDir=graphDir,graphName=graphName,...)
+    r2lUniv(data=data,textBefore=textBefore,textAfter=textAfter,graphDir=graphDir,graphName=graphName,...)
 }
 
-r2lUniv <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
   UseMethod("r2lUniv")
 }
 
-r2lUniv.logical <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.logical <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     r2lUniv.factor(data=data,textBefore=textBefore,graphDir=graphDir,graphName=graphName,...)
 }
 
-r2lUniv.caracter <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.caracter <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     r2lUniv.factor(data=data,textBefore=textBefore,graphDir=graphDir,graphName=graphName,...)
 }
 
-r2lUniv.factor <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.factor <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     cat("\n\n%%%% r2lu.factor %%%%\n")
     cat(textBefore,"\n")
     r2lBeginTitle(data)
     r2lFrequency(data,"&\n")
     r2lBarplot(data,"\\\\ \\hline \n",graphDir,graphName)
     r2lEnd()
+    cat(textAfter,"\n")
 }
 
-r2lUniv.ordered <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.ordered <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     cat("\n\n%%%% r2lu.ordered %%%%\n")
     cat(textBefore,"\n")
     r2lBeginTitle(data)
@@ -38,9 +39,10 @@ r2lUniv.ordered <- function(data,textBefore="",graphDir="graphUniv",graphName="V
     r2lSummary(data,"&\n")
     r2lBarplot(data," \\\\ \\hline \n",graphDir,graphName)
     r2lEnd()
+    cat(textAfter,"\n")
 }
 
-r2lUniv.integer <- r2lUniv.numeric <- function(data,textBefore="",graphDir="graphUniv",graphName="V",limDiscrete=10,...){
+r2lUniv.integer <- r2lUniv.numeric <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",limDiscrete=10,...){
     if(length(table(data))<=limDiscrete && identical(as.numeric(data),round(data))){
       class(data) <- c("discrete",class(data))
     }else{
@@ -49,7 +51,7 @@ r2lUniv.integer <- r2lUniv.numeric <- function(data,textBefore="",graphDir="grap
     r2lUniv(data=data,textBefore=textBefore,graphDir=graphDir,graphName=graphName,...)
 }
 
-r2lUniv.discrete <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.discrete <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     cat("\n\n%%%% r2lu.discrete %%%%\n")
     cat(textBefore,"\n")
     r2lBeginTitle(data)
@@ -58,9 +60,10 @@ r2lUniv.discrete <- function(data,textBefore="",graphDir="graphUniv",graphName="
     r2lBoxplot(data," & \n",graphDir,graphName)
     r2lBarplot(data,"\\\\ \\hline \n",graphDir,graphName)
     r2lEnd()
+    cat(textAfter,"\n")
 }
 
-r2lUniv.continuous <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.continuous <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     cat("\n\n%%%% r2lu.continuous %%%%\n")
     cat(textBefore,"\n")
     r2lBeginTitle(data)
@@ -68,9 +71,10 @@ r2lUniv.continuous <- function(data,textBefore="",graphDir="graphUniv",graphName
     r2lBoxplot(data,"&\n",graphDir,graphName)
     r2lHist(data,"\\\\ \\hline \n",graphDir,graphName)
     r2lEnd()
+    cat(textAfter,"\n")
 }
 
-r2lUniv.data.frame <- function(data,textBefore="",graphDir="graphUniv",graphName="V",...){
+r2lUniv.data.frame <- function(data,textBefore="",textAfter="",graphDir="graphUniv",graphName="V",...){
     nbVar <- length(data)
     if(length(textBefore)==1){textBefore <- rep(textBefore,time=nbVar)}else{}
     if(length(graphName)==1){graphName <- paste(graphName,1:nbVar,sep="")}else{}
